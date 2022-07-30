009d75be5999f729c82e94018ada819e8e255989
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2022-07-30 02:33:05 -0400

    Database Authentication

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,20 +1,32 @@
 package com.example.demo.security;
 
 
+import com.example.demo.auth.ApplicationUser;
+import com.example.demo.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -22,9 +34,11 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
+        this.applicationUserService = applicationUserService;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -42,9 +56,8 @@ public class ApplicationSecurityConfig {
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
-                .passwordParameter("password")
                 .usernameParameter("username")
-
+                .passwordParameter("password")
                 .and()
                 .rememberMe() // 2 weeks
 //                .tokenRepository() // This is for database
@@ -65,7 +78,7 @@ public class ApplicationSecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public UserDetailsManager userDetailsService() {
         UserDetails annasmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
@@ -85,5 +98,53 @@ public class ApplicationSecurityConfig {
                 .authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
                 .build();
         return new InMemoryUserDetailsManager(annasmithUser, lindaUser, tomUser);
+    }
+
+//    @Bean
+//    public DataSource dataSource(){
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+//                .build();
+//    }
+//
+//    @Bean
+//    public UserDetailsManager userDetailsService(DataSource dataSource){
+//        UserDetails annasmithUser = User.builder()
+//                .username("annasmith")
+//                .password(passwordEncoder.encode("password"))
+////                .roles(ApplicationUserRole.STUDENT.name()) // ROLE_STUDENT
+//                .authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
+//                .build();
+//        UserDetails lindaUser = User.builder()
+//                .username("linda")
+//                .password(passwordEncoder.encode("password"))
+////                .roles(ApplicationUserRole.ADMIN.name())
+//                .authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
+//                .build();
+//        UserDetails tomUser = User.builder()
+//                .username("tom")
+//                .password(passwordEncoder.encode("password"))
+////                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+//                .authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+//        users.createUser(annasmithUser);
+//        users.createUser(lindaUser);
+//        users.createUser(tomUser);
+//        return users;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
